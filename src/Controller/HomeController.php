43c7863a440f51b87be654c2fdb70f565321b1b0
mkd-0 <mahajoub_kaled@yahoo.fr>
2024-02-22 03:42:56 +0100

    add service section

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Service;
+use App\Entity\Testimony;
+// use App\Controller\TestimonyType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\TestimonyType;
+//use App\Controller\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -15,10 +21,12 @@ class HomeController extends AbstractController
     public function index(EntityManagerInterface $entityManager): Response
     {
         $services = $entityManager->getRepository(Service::class)->findAll();
+        $testimonies = $entityManager->getRepository(Testimony::class)->findAll();
 
         return $this->render('home/index.html.twig', [
 
-            'services' => $services
+            'services' => $services,
+            'testimonies' => $testimonies
         ]);
     }
 }
